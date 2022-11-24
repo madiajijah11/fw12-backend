01ec00c570147fb28a04cb3c3c8f94890b4b2ec0
MexL <madiajijah7@gmail.com>
2022-11-24 11:14:16 +0800

    Update users.js

--- src/models/users.js
+++ src/models/users.js
@@ -1,7 +1,9 @@
 const pool = require("../helpers");
 
 exports.getUsers = (req, res) => {
-  pool.query("SELECT * FROM users", (err, result) => {
+  const selectQueryAll = "SELECT * FROM users";
+
+  pool.query(selectQueryAll, (err, result) => {
     if (err) {
       return res.status(400).json({
         success: false,
@@ -17,7 +19,10 @@ exports.getUsers = (req, res) => {
 
 exports.getUser = (req, res) => {
   const { id } = req.params;
-  pool.query(`SELECT * FROM users WHERE id = ${id}`, (err, result) => {
+
+  const selectQueryById = `SELECT * FROM users WHERE id = ${id}`;
+
+  pool.query(selectQueryById, (err, result) => {
     if (err) {
       return res.status(400).json({
         success: false,
@@ -34,47 +39,48 @@ exports.getUser = (req, res) => {
 exports.createUser = (req, res) => {
   const { picture, firstName, lastName, phoneNumber, email, password, roleId } =
     req.body;
-  pool.query(
-    `INSERT INTO users ("picture", "firstName", "lastName", "phoneNumber", "email", "password", "roleId") VALUES ('${picture}', '${firstName}', '${lastName}', '${phoneNumber}',  '${email}', '${password}', '${roleId}')`,
-    (err, _result) => {
-      if (err) {
-        return res.status(400).json({
-          success: false,
-          message: err,
-        });
-      }
-      return res.status(200).json({
-        success: true,
-        message: "User created successfully",
+
+  const insertQuery = `INSERT INTO users ("picture", "firstName", "lastName", "phoneNumber", "email", "password", "roleId") VALUES ('${picture}', '${firstName}', '${lastName}', '${phoneNumber}',  '${email}', '${password}', '${roleId}')`;
+
+  pool.query(insertQuery, (err, _result) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        message: err,
       });
     }
-  );
+    return res.status(200).json({
+      success: true,
+      message: "User created successfully",
+    });
+  });
 };
 
 exports.updateUser = (req, res) => {
   const { id } = req.params;
   const { picture, firstName, lastName, phoneNumber, email, password, roleId } =
     req.body;
-  pool.query(
-    `UPDATE users SET "picture" = '${picture}', "firstName" = '${firstName}', "lastName" = '${lastName}', "phoneNumber" = '${phoneNumber}', "email" = '${email}', "password" = '${password}', "roleId" = '${roleId}' WHERE "id" = ${id}`,
-    (err, _result) => {
-      if (err) {
-        return res.status(400).json({
-          success: false,
-          message: err,
-        });
-      }
-      return res.status(200).json({
-        success: true,
-        message: "User updated successfully",
+
+  const updateQuery = `UPDATE users SET "picture" = '${picture}', "firstName" = '${firstName}', "lastName" = '${lastName}', "phoneNumber" = '${phoneNumber}', "email" = '${email}', "password" = '${password}', "roleId" = '${roleId}' WHERE "id" = ${id}`;
+
+  pool.query(updateQuery, (err, _result) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        message: err,
       });
     }
-  );
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+    });
+  });
 };
 
 exports.deleteUser = (req, res) => {
   const { id } = req.params;
-  pool.query(`DELETE FROM users WHERE id = ${id}`, (err, _result) => {
+  const deleteQuery = `DELETE FROM users WHERE id = ${id}`;
+  pool.query(deleteQuery, (err, _result) => {
     if (err) {
       return res.status(400).json({
         success: false,
