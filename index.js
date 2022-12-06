8e0000972d2d3203a826072278d5057e4ad0335a
MexL <madiajijah7@gmail.com>
2022-12-06 22:52:28 +0800

    disable helmet

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(helmet());
+app.use(cors());
+// app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 if (process.env.APP_DEBUG === "development") {
   app.use(morgan("dev"));
