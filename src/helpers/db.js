51ce035c2f72c4452a19ece601a55b3cbbcc45d1
MexL <madiajijah7@gmail.com>
2022-12-09 18:31:04 +0800

    env change

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require("pg");
 
+require("dotenv").config();
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -9,7 +11,7 @@ const pool = new Pool({
 });
 
 const poolString = new Pool({
-  connectionString: "postgres://postgres:1@localhost:5432/mexl_cinema",
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = { pool, poolString };
